1e40b1e587ab6060b6d71263dd6a96ab66286f65
Noddito <noddito@gmail.com>
2024-03-20 17:00:03 +0500

    Added admin pannel

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
 
 class Service extends Model
 {
     use HasFactory;
+    use AsSource;
+
     protected $table = 'services';
 }
---
+++ resources/views/inc/img.blade.php
@@ -0,0 +1,3 @@
+<img src='@yield('src')'
+alt=''
+class='mw-100 d-block img-fluid rounded-1 w-100'>
